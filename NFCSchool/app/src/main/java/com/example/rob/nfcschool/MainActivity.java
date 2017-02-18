b32a0e8328f48da969e26701476dfb771475e97a
MikeMaty <mikematy@hotmail.ca>
2017-02-18 14:30:22 -0500

    read/write file

--- NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
+++ NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.rob.nfcschool;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     static String nfcRead;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,13 +54,55 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    private String[] splitReadMessage(String message){
+
+    ArrayList<String> messageList = new ArrayList<String>();
+
+    private String[] splitReadMessage(String message) {
         String splitMessage[] = message.split(">", 1);
+        messageList.add(splitMessage[1]);
         return splitMessage;
     }
-    String splitMessage[] = splitReadMessage(nfcRead);
-    String courseName = splitMessage[0];
-    String homework = splitMessage[1];
+
+    private void writeToFile(String myData) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("totalMessage.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(myData);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.v("MyActivity", e.toString());
+        }
+    }
+
+    private String readFromFile() {
+        String result = "";
+
+        try {
+            InputStream inputStream = openFileInput("totalMessage.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String tempString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (tempString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(tempString);
+                }
+                inputStream.close();
+                result = stringBuilder.toString();
+            }
+        } catch (FileNotFoundException e) {
+            Log.v("MyActivity", "File not found" + e.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 }
 
