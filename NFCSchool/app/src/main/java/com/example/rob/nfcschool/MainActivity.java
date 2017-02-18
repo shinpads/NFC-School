0a8732d13019ffeb519a021346b1683598cc60ed
marshmelllo <robfarlow@gmail.com>
2017-02-18 12:09:04 -0500

    nfc read and save input

--- NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
+++ NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.rob.nfcschool;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    static String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO read NFC tag
-            }
-        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+        if(fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO read NFC tag
+                    Intent intent = new Intent(MainActivity.this,ReadTag.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
     }
 }
