c8e8a41535158a1494564d9eeac8d6d132b8d393
MikeMaty <mikematy@hotmail.ca>
2017-02-18 10:31:04 -0500

    Created Split Message Function

--- NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
+++ NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    String nfcRead = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO read NFC tag
+                //TODO read NFC
+
             }
         });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private String[] splitReadMessage(String message){
+        String splitMessage[] = message.split(">", 1);
+        return splitMessage;
+    }
+    String splitMessage[] = splitReadMessage(nfcRead);
+    String courseName = splitMessage[0];
+    String homework = splitMessage[1];
 }
