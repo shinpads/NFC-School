448ddece82374f37094ef0dabbd9830bd7ce17a9
marshmelllo <robfarlow@gmail.com>
2017-02-18 09:40:49 -0500

    Setup ReadTag Activity

--- NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
+++ NFCSchool/app/src/main/java/com/example/rob/nfcschool/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.rob.nfcschool;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO read NFC tag
+            }
+        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ NFCSchool/app/src/main/java/com/example/rob/nfcschool/ReadTag.java
@@ -0,0 +1,13 @@
+package com.example.rob.nfcschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//ACTIVITY TO READ TAG AND GET INFORMATION
+public class ReadTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_tag);
+    }
+}
